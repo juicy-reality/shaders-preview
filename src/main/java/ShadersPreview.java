6f8b04913a3f6a1df461e5f07fde73ac6233c4f7
Denys Potapov <denys.potapov@gmail.com>
2017-06-17 21:41:25 +0300

    multiple pixel shader

--- src/main/java/ShadersPreview.java
+++ src/main/java/ShadersPreview.java
@@ -381,28 +381,28 @@ public class ShadersPreview {
         // Do a complete rotation every 10 seconds.
         long time = System.currentTimeMillis() % 5000L - 2500L;
         float angleInDegrees = (-90.0f / 10000.0f) * ((int) time);
-        System.out.println("recalculatiing "+ angleInDegrees);
+        // System.out.println("recalculatiing "+ angleInDegrees);
         mModelMatrix = Matrix.setIdentityM(mModelMatrix, 0);
-        mModelMatrix = Matrix.rotateM(mModelMatrix, 0, 5, 1.0f, 0.0f, 0.0f);
+        mModelMatrix = Matrix.rotateM(mModelMatrix, 0, 30, 1.0f, 0.0f, 0.0f);
 //        System.out.println("Current mModelMatrix" + Arrays.toString(mModelMatrix));
 
 //        System.out.println("Current mModelMatrix" + Arrays.toString(mModelMatrix));
         mModelMatrix = Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 1.0f, 0.0f);
         mModelMatrix = Matrix.translateM(mModelMatrix, 0, 0.0f, 0.0f, 1.0f);
 
-        System.out.println("Current mModelMatrix" + Arrays.toString(mModelMatrix));
+        // System.out.println("Current mModelMatrix" + Arrays.toString(mModelMatrix));
 
         // This multiplies the view matrix by the model matrix, and stores the result in the MVP matrix
         // (which currently contains model * view).
         mMVPMatrix = Matrix.multiplyMM(mMVPMatrix, 0, mViewMatrix, 0, mModelMatrix, 0);
 
         mProjectionMatrix = Matrix.frustumM(mProjectionMatrix, 0, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 40.0f);
-        System.out.println("Current mProjectionMatrix" + Arrays.toString(mProjectionMatrix));
+        // System.out.println("Current mProjectionMatrix" + Arrays.toString(mProjectionMatrix));
 
         // This multiplies the modelview matrix by the projection matrix, and stores the result in the MVP matrix
         // (which now contains model * view * projection).
         mMVPMatrix = Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0);
-        System.out.println("Current mMVPMatrix" + Arrays.toString(mMVPMatrix));
+        // System.out.println("Current mMVPMatrix" + Arrays.toString(mMVPMatrix));
 //        try {
 //            TimeUnit.SECONDS.sleep(1);
 //        } catch (InterruptedException e) {
--- src/main/resources/draw.frag
+++ src/main/resources/draw.frag
@@ -1,15 +1,19 @@
 #version 150 core
 precision mediump float;
 
+#define WIDTH 640.0
+#define HEIGHT 480.0
+
 uniform sampler2D uSourceTex;
 uniform sampler2D uSourceTex2;
 // uniform mat4 u_MVPMatrix;
-
 in vec2 texcoord;
 out vec4 color;
 
 void main(void)                    // The entry point for our fragment shader.
 {
+    float dy = 1.0 / HEIGHT;
+    float dx = 1.0 / WIDTH;
 
     vec4 disp = texture(uSourceTex, texcoord);
     //color=disp;
